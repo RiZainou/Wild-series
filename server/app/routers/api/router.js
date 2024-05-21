51226d54d8402273b4c233868d4f57f453c68cc0
RiZainou <m.toun976@gmail.com>
2024-05-21 15:30:44 +0200

    add programs route

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -4,11 +4,18 @@ const router = express.Router();
 
 /* ************************************************************************* */
 // Import And Use Routers Here
+const { sayWelcome } = require("../../controllers/sayActions");
+
+router.get("/", sayWelcome);
 /* ************************************************************************* */
 
 const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
+
+const programsRouters = require("./programs/router");
+
+router.use("/programs", programsRouters);
 
 /* ************************************************************************* */
 
